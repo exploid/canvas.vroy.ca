071230f202b9a67a49e4ffaaeb45e1fd091cf056
Vincent Roy <vincentroy8@gmail.com>
2011-04-07 00:23:01 -0300

    Actually add support to log clicks properly and a separate action to display the heatmap.

--- app.rb
+++ app.rb
@@ -2,10 +2,29 @@ require "rubygems"
 require "ramaze"
 require "json"
 
+Coordinates = []
+
 class MainController < Ramaze::Controller
   map '/'
   
   def index
-    "Heatmap"
   end
+
+  def heatmap
+    @divs = ""
+
+    Coordinates.each do |x, y|
+      @divs << %(<div class="point" style="left:#{x}; top:#{y};"></div>\n)
+    end
+
+    render_template("index.xhtml")
+  end
+
+  def click(x, y)
+    x, y = x.to_i, y.to_i
+    if x != 0 and y != 0
+      Coordinates << [ x, y ]
+    end
+  end
+
 end
