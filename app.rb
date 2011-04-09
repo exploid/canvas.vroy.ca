d922973a60760f4fd340fb914fa7f9a43c24e907
Vincent Roy <vincentroy8@gmail.com>
2011-04-09 17:45:13 -0300

    Add persistence to the canvases.

--- app.rb
+++ app.rb
@@ -6,7 +6,30 @@ require "ramaze"
 require "json"
 require "juggernaut"
 
-Coordinates = {}
+require "sequel"
+
+DB = Sequel.connect("mysql://root:asdf@localhost/canvas")
+
+unless DB.table_exists? :coordinates
+  DB.create_table :coordinates do
+    primary_key :id
+    String :canvas
+    String :x
+    String :y
+    String :color
+  end
+end
+
+class Coordinate < Sequel::Model(DB[:coordinates])
+  def self.clear(canvas)
+    self.filter(:canvas => canvas).delete
+  end
+  def self.load(canvas)
+    DB["SELECT * FROM canvas.coordinates WHERE canvas=?", canvas].map do |coord|
+      [ coord[:x], coord[:y], coord[:color] ]
+    end
+  end
+end
 
 class MainController < Ramaze::Controller
   map '/'
@@ -15,33 +38,33 @@ class MainController < Ramaze::Controller
     @canvas = canvas
   end
   
-  deny_layout :load_coordinates
+  deny_layout :load
   def load(canvas)
-    coordinates = []
-    if !Coordinates[canvas].to_a.empty?
-      coordinates = Coordinates[canvas].first
-    end
-    return coordinates.to_json
+    return Coordinate.load(canvas).to_json
   end
 
   deny_layout :clear
   def clear(canvas)
-    Coordinates[canvas] = []
+    Coordinate.clear(canvas)
     Juggernaut.publish( canvas, { :action => "clear" } )
     return true.to_json
   end
 
   # Action to receive clicks via AJAX posts.
-  # Expects an array of coordinates in the "clicks" key: [ [x1,y1], [x2,y2], [x3,y3] ]
+  # Expects an array of coordinates in the "clicks" key: [ [x1,y1,color], [x2,y2,color], [x3,y3,color] ]
   # Expects a string to identify the canvas in the "canvas" key.
   deny_layout :click
   def click
     canvas = request[:canvas]
-    Coordinates[canvas] ||= []
 
     clicks = request[:clicks].map{|x| x.last }
-
-    Coordinates[canvas] << clicks
+    clicks.each do |click|
+      Coordinate.create(:canvas => canvas,
+                        :x => click[0],
+                        :y => click[1],
+                        :color => click[2])
+    end
+      
     Juggernaut.publish(canvas, clicks, :except => request.env["HTTP_X_SESSION_ID"])
   end
 
